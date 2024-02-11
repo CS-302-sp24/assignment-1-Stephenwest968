40e71503450b933af9daa377f8352032cf54d364
Stephenwest968 <143018045+Stephenwest968@users.noreply.github.com>
2024-02-11 12:51:12 -0600

    Assignment 1 Submission
    This is the updated files for the  Dining Philosophers problem. I have included my Trace.txt file with the outputs to 4 different scenarios.

--- Assignment 1/Philosopher.java
+++ Assignment 1/Philosopher.java
@@ -4,23 +4,93 @@ class Philosopher extends Thread {
   private Chopstick left, right;
   private Random random;
   private int thinkCount;
+  private int maxThink;
+  private int maxEat;
+  private int cycleCount;
+  private String start;
 
-  public Philosopher(Chopstick left, Chopstick right) {
-    this.left = left; this.right = right;
+  public Philosopher(Chopstick left, Chopstick right, int maxThink, int maxEat, int cycleCount, String start) {
+    this.left = left; this.right = right; this.maxThink = maxThink; this.maxEat = maxEat; this.cycleCount = cycleCount; this.start = start;
     random = new Random();
   }
 
   public void run() {
     try {
-      while(true) {
-        ++thinkCount;
-        if (thinkCount % 10 == 0)
-          System.out.println("Philosopher " + this + " has thought " + thinkCount + " times");
-        Thread.sleep(random.nextInt(1000));     // Think for a while
-        synchronized(left) {                    // Grab left chopstick 
-          synchronized(right) {                 // Grab right chopstick 
-            Thread.sleep(random.nextInt(1000)); // Eat for a while
+      if (cycleCount == 0){ //if cycleCount is 0, then will run forever
+        while(true) {
+          ++thinkCount;
+          if (thinkCount % 10 == 0){
+            System.out.println("Philosopher " + this + " has thought " + thinkCount + " times");
+          int TempNum = random.nextInt(maxThink);
+          Thread.sleep(TempNum);     // Think for a while
+          System.out.println("Philosopher " + this + "has thought for " + TempNum  + " units");
+
+          if (start == "left"){ //if left handed, start with left
+            System.out.println("Philosopher " + this + "wants left chopstick");
+            synchronized(left) {                    // Grab left chopstick 
+              System.out.println("Philosopher " + this + "wants right chopstick");
+              synchronized(right) {                 // Grab right chopstick 
+                int tempEat = random.nextInt(maxEat);
+                Thread.sleep(tempEat);
+                System.out.println("Philosopher" + this + "has eaten for " + tempEat + "units");
+               } // Eat for a while
+               System.out.println("Philosopher" + this + "has realease right chopstick");
+            }
+            System.out.println("Philosopher" + this + "has realease left chopstick");
+      
           }
+          else{ //if right handed, start with right
+            System.out.println("Philosopher " + this + "wants right chopstick");
+            synchronized(right) {                    // Grab left chopstick 
+              System.out.println("Philosopher " + this + "wants left chopstick");
+              synchronized(left) {                 // Grab right chopstick 
+                int tempEat = random.nextInt(maxEat);
+                Thread.sleep(tempEat);
+                System.out.println("Philosopher" + this + "has eaten for " + tempEat + "units");
+               } // Eat for a while
+               System.out.println("Philosopher" + this + "has realease left chopstick");
+            }
+            System.out.println("Philosopher" + this + "has realease right chopstick");
+          }
+          }
+        }
+      }
+      else{
+        while(cycleCount >= 1) { //if cycleCount is greater than 1, run number of times declared as cycleCount
+          ++thinkCount;
+          if (thinkCount % 10 == 0)
+            System.out.println("Philosopher " + this + " has thought " + thinkCount + " times");
+            int TempNum = random.nextInt(maxThink);
+            Thread.sleep(TempNum);     // Think for a while
+            System.out.println("Philosopher " + this + "has thought for " + TempNum  + " units");
+
+          if (start == "left"){ //if left handed, start with left
+            System.out.println("Philosopher " + this + "wants left chopstick");
+            synchronized(left) {                    // Grab left chopstick 
+              System.out.println("Philosopher " + this + "wants right chopstick");
+              synchronized(right) {                 // Grab right chopstick 
+                int tempEat = random.nextInt(maxEat);
+                Thread.sleep(tempEat);
+                System.out.println("Philosopher" + this + "has eaten for " + tempEat + "units");
+               } // Eat for a while
+               System.out.println("Philosopher" + this + "has realease right chopstick");
+            }
+            System.out.println("Philosopher" + this + "has realease left chopstick");
+            }
+          else{ //if right handed, start with right
+            System.out.println("Philosopher " + this + "wants right chopstick");
+            synchronized(right) {                    // Grab left chopstick 
+              System.out.println("Philosopher " + this + "wants left chopstick");
+              synchronized(left) {                 // Grab right chopstick 
+                int tempEat = random.nextInt(maxEat);
+                Thread.sleep(tempEat);
+                System.out.println("Philosopher" + this + "has eaten for " + tempEat + "units");
+               } // Eat for a while
+               System.out.println("Philosopher" + this + "has realease left chopstick");
+            }
+            System.out.println("Philosopher" + this + "has realease right chopstick");
+          }
+          cycleCount -= 1;
         }
       }
     } catch(InterruptedException e) {}
